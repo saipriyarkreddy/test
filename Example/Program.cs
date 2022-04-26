87dc13ca12d4676b1ada20ab3090b47ba3f32c69
Sri Priya <Sri Priya@SAIPRIYA>
2022-04-26 01:23:47 -0700

    Add project files.

---
+++ Example/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MyFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
